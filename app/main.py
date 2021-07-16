70f8a2abc5e877a28d9ab5d76971cc5e77f1f6ff
pankaj1102 <pa1102@hotmail.com>
2021-07-16 14:16:51 +1000

    first commit

---
+++ app/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from starlette.responses import JSONResponse
+from joblib import load
+import pandas as pd
+
+app = FastAPI()
+
+rfc_pipeline = load("pipeline_model_rfc.joblib")
+
+@app.get("/")
+def read_root():
+    return {"Beer Style Prediction. Usage: /health -> Check cluster health. /beer/predictstyle?<params> -> Generate Prediction. /beer/stylesPred?<params> -> Generate predictions for a list of inputs."}
+
+@app.get("/health")
+def healthcheck():
+    return 'Beer Prediction application is online and ready to go!'
+
+def format_features(brewery_name: str, rev_aroma: float, rev_taste: float, rev_appearance: float, rev_palate: float):
+    return {
+        "brewery_name": [brewery_name],
+        "review_aroma": [rev_aroma],
+        "review_taste": [rev_taste],
+        "review_appearance": [rev_appearance],
+        "review_palate": [rev_palate]
+    }
+
+@app.get("/beer/predictstyle")
+def predict(brewery_name: str, rev_aroma: float, rev_taste: float, rev_appearance: float, rev_palate: float):
+    from feature_encode import BeerStyleCode
+    beer_style_decode = BeerStyleCode()
+    features = format_features(brewery_name, rev_aroma, rev_taste, rev_appearance, rev_palate)
+    obs = pd.DataFrame(features)
+    pred = beer_style_decode.inverse_transform(rfc_pipeline.predict(obs))
+    return JSONResponse(pred.tolist())
+
+@app.get("/beer/stylespred")
+def predict(brewery_name: str, rev_aroma: float, rev_taste: float, rev_appearance: float, rev_palate: float):
+    features = format_features(brewery_name, rev_aroma, rev_taste, rev_appearance, rev_palate)
+    obs = pd.DataFrame(features)
+    pred = rfc_pipeline.predict(obs)
+    return JSONResponse(pred.tolist())
+
